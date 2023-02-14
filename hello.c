856343301984efc7e9ab11532071923d7be8ea0c
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-14 15:02:10 +0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,36 @@
+#define _USE_MATH_DEFINES
+#include <malloc.h>
 #include <stdio.h>
+#include <math.h>
+#define N 10000000
+
+void func(double** my_array, int len)
+{
+    double x = 2 * M_PI / N;
+    double* temp = (double*)malloc(sizeof(double) * len);
+    for (int i = 0; i < len; ++i)
+    {
+        temp[i] = sin(i*x);
+    }
+    *my_array = temp;
+}
+double summ(double** my_array, int len)
+{
+    double sum = 0;
+    double* temp = *my_array;
+    for (int i = 0; i < len; ++i)
+    {
+        sum += temp[i];
+    }
+    return sum;
+}
+
 int main()
 {
-	printf("hello, world");
-	return 0;
+    double** array = (double**)malloc(sizeof(double**));
+    long long len = N;
+    func(array, len);
+    double sum = summ(array, len);
+    printf("%lf", sum);
+    return 0;
 }
