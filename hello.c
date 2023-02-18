a939cc99f6ac8a8bd44b5d2c8dae164b02d857d4
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-19 03:36:38 +0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,44 +3,30 @@
 #include <math.h>
 #define N 10000000
 #define M_PI 3.14159265358979323846
-void func(double** my_array, int len)
-{
-    double x = 2 * M_PI / N;
-    double* temp = (double*)malloc(sizeof(double) * len);
-#pragma acc parallel num_gangs(2048) vector_length(224) copyout(temp[:len])
-{
-#pragma acc loop gang vector
-{
-                for (int i = 0; i < len; ++i)
-                {
-                    temp[i] = sin(i * x);
-                }
-}
-}
-    *my_array = temp;
-}
-double summ(double** my_array, int len)
-{
-    double sum = 0;
-    double* temp = *my_array;
-#pragma acc parallel num_gangs(2048) vector_length(224) copyout(sum) copyin(temp[:len])
-{
-#pragma acc loop gang vector
-{
-                for (int i = 0; i < len; ++i)
-                {
-                    sum += temp[i];
-                }
-}
-}
-    return sum;
-}
 
 int main()
 {
-    double** array = (double**)malloc(sizeof(double**));
-    func(array, N);
-    printf("%lf", summ(array, N));
+    //double** array = (double**)malloc(sizeof(double**));
+    double x = 2 * M_PI / N;
+    double sum = 0;
+#pragma acc parallel num_gangs(2048) vector_length(224)  copyout(sum) 
+    {
+#pragma acc loop gang vector
+        {
+            double* temp = (double*)malloc(sizeof(double) * len);
+            for (int i = 0; i < len; ++i)
+            {
+                temp[i] = sin(i * x);
+            }
+
+            for (int i = 0; i < len; ++i)
+            {
+                sum += temp[i];
+            }
+        }
+
+    }
+    printf("%lf", sum);
     free(*array);
     return 0;
 }
