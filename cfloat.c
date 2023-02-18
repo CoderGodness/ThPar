df8d37d5ba1c17671f6bf69fcb484b6ac29801fa
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-19 03:52:43 +0700

    Create cfloat.c

---
+++ cfloat.c
@@ -0,0 +1,38 @@
+#include <malloc.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>       // for clock_t, clock(), CLOCKS_PER_SEC
+#include <unistd.h>
+#define N 10000000
+#define M_PI 3.14159265358979323846
+void func(float** my_array, int len)
+{
+    float x = 2 * M_PI / N;
+    float* temp = (float*)malloc(sizeof(float) * len);
+      for (int i = 0; i < len; ++i)
+      {
+          temp[i] = sin(i * x);
+      }
+    *my_array = temp;
+}
+{
+    float sum = 0;
+    float* temp = *my_array;
+			for (int i = 0; i < len; ++i)
+			{
+					sum += temp[i];
+			}
+    return sum;
+}
+
+int main()
+{
+ 		clock_t begin = clock();
+    float** array = (float**)malloc(sizeof(float**));
+    func(array, N);
+    printf("%lf", summ(array, N));
+    free(*array);
+		clock_t end = clock();
+		printf("The elapsed time is %f seconds", (double)(end - begin) / CLOCKS_PER_SEC);
+    return 0;
+}
