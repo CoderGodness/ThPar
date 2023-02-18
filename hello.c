0377a7fa524b4e5f5779dea88a890fc2df7bf91d
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-19 03:40:32 +0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,42 +3,10 @@
 #include <math.h>
 #define N 10000000
 #define M_PI 3.14159265358979323846
-void func(double** my_array, int len)
-{
-    double x = 2 * M_PI / N;
-    double* temp = (double*)malloc(sizeof(double) * len);
-#pragma acc parallel num_gangs(2048) vector_length(224) copyout(temp[:len])
-    {
-#pragma acc loop gang vector
-        {
-            for (int i = 0; i < len; ++i)
-            {
-                temp[i] = sin(i * x);
-            }
-        }
-    }
-    *my_array = temp;
-}
-double summ(double** my_array, int len)
-{
-    double sum = 0;
-    double* temp = *my_array;
-#pragma acc parallel num_gangs(2048) vector_length(224) copyout(sum) copyin(temp[:len])
-    {
-#pragma acc loop gang vector
-        {
-            for (int i = 0; i < len; ++i)
-            {
-                sum += temp[i];
-            }
-        }
-    }
-    return sum;
-}
-
 int main()
 {
     double x = 2 * M_PI / N;
+    int len = N;
     double* temp = (double*)malloc(sizeof(double) * len);
 #pragma acc parallel num_gangs(2048) vector_length(224) copyout(temp[:len])
     {
@@ -51,7 +19,6 @@ int main()
         }
     }
     double sum = 0;
-    double* temp = *my_array;
 #pragma acc parallel num_gangs(2048) vector_length(224) copyout(sum) copyin(temp[:len])
     {
 #pragma acc loop gang vector
