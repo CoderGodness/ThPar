16f7380d116629c505484ea46c72e0d8e95e818d
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-19 03:43:10 +0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,13 +3,11 @@
 #include <math.h>
 #define N 10000000
 #define M_PI 3.14159265358979323846
-int main()
+void func(double** my_array, int len)
 {
     double x = 2 * M_PI / N;
-    int len = N;
     double* temp = (double*)malloc(sizeof(double) * len);
-    double sum = 0;
-#pragma acc parallel num_gangs(2048) vector_length(224)
+#pragma acc parallel num_gangs(2048) vector_length(224) copyout(temp[:len])
     {
 #pragma acc loop gang vector
         {
@@ -17,12 +15,32 @@ int main()
             {
                 temp[i] = sin(i * x);
             }
-             for (int i = 0; i < len; ++i)
+        }
+    }
+    *my_array = temp;
+}
+double summ(double** my_array, int len)
+{
+    double sum = 0;
+    double* temp = *my_array;
+#pragma acc parallel num_gangs(2048) vector_length(224) copyout(sum) copyin(temp[:len])
+    {
+#pragma acc loop gang vector
+        {
+            for (int i = 0; i < len; ++i)
             {
                 sum += temp[i];
             }
         }
     }
-    printf("%lf", sum);
+    return sum;
+}
+
+int main()
+{
+    double** array = (double**)malloc(sizeof(double**));
+    func(array, N);
+    printf("%lf", summ(array, N));
+    free(*array);
     return 0;
 }
