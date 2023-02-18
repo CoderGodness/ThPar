e497c7a0c87a6689a2196c1183674ea864c9a669
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-19 03:45:50 +0700

    Create float.c

---
+++ float.c
@@ -0,0 +1,46 @@
+#include <malloc.h>
+#include <stdio.h>
+#include <math.h>
+#define N 10000000
+#define M_PI 3.14159265358979323846
+void func(float** my_array, int len)
+{
+    float x = 2 * M_PI / N;
+    float* temp = (float*)malloc(sizeof(float) * len);
+#pragma acc parallel num_gangs(2048) vector_length(224) copyout(temp[:len])
+    {
+#pragma acc loop gang vector
+        {
+            for (int i = 0; i < len; ++i)
+            {
+                temp[i] = sin(i * x);
+            }
+        }
+    }
+    *my_array = temp;
+}
+float summ(float** my_array, int len)
+{
+    float sum = 0;
+    float* temp = *my_array;
+#pragma acc parallel num_gangs(2048) vector_length(224) copyout(sum) copyin(temp[:len])
+    {
+#pragma acc loop gang vector
+        {
+            for (int i = 0; i < len; ++i)
+            {
+                sum += temp[i];
+            }
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    float** array = (float**)malloc(sizeof(float**));
+    func(array, N);
+    printf("%f", summ(array, N));
+    free(*array);
+    return 0;
+}
