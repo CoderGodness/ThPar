04f8b2ad02e50130cb79312989167c0da450a3e2
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-21 14:28:35 +0700

    Update coredouble.c

--- coredouble.c
+++ coredouble.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>       // for clock_t, clock(), CLOCKS_PER_SEC
-#include <unistd.h>
 #define N 10000000
 #define M_PI 3.14159265358979323846
 void func(double** my_array, int len)
