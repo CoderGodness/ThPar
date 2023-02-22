ef8a68721b5093cd1b50e170341c4d0ebb221a1a
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-22 16:28:29 +0700

    Update double.c

--- double.c
+++ double.c
@@ -3,10 +3,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h> 
-clock_t beginfill;
-clock_t endfill;
-clock_t beginsum;
-clock_t endsum;
+
 int main()
 {
     clock_t begin = clock();
@@ -16,7 +13,6 @@ int main()
     double sum = 0;
     #pragma acc data create(arr[0:len]) copyout(sum)
     {
-        beginfill = clock();
         #pragma acc parallel num_gangs(2048) vector_length(256)
         {
             for (int i = 0; i < len; ++i)
@@ -24,9 +20,6 @@ int main()
                 arr[i] = sin(i * x);
             }
         }
-        endfill = clock();
-    
-        beginsum = clock();
         #pragma acc parallel num_gangs(2048) vector_length(256)
         {
             for (int i = 0; i < len; ++i)
@@ -34,13 +27,10 @@ int main()
                 sum += arr[i];
             }
         }
-        endsum = clock();
     }
     free(arr);
     clock_t end = clock();
     printf("%lf",sum);
     printf("\nThe elapsed time is %lf seconds", (double)(end - begin) / CLOCKS_PER_SEC);
-     printf("\nFill cycle running time = %lf seconds", (double)(endfill - beginfill) / CLOCKS_PER_SEC);
-    printf("\nSum cycle running time = %lf seconds", (double)(endsum - beginsum) / CLOCKS_PER_SEC);
     return 0;
 }
