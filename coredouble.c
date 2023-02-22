eb9c5afa87901a9e79b5626b3c2bc150f0a5d9a4
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-22 13:53:05 +0700

    Update coredouble.c

--- coredouble.c
+++ coredouble.c
@@ -14,24 +14,30 @@ int main()
    // #pragma acc data create(arr[0:len]) copyout(sum)
     {
         //#pragma acc parallel num_gangs(2048) vector_length(256)
+        clock_t beginfill = clock();
         {
             for (int i = 0; i < len; ++i)
             {
                 arr[i] = sin(i * x);
             }
         }
+        clock_t endfill = clock();
     
       //  #pragma acc parallel num_gangs(2048) vector_length(256)
+        clock_t beginsum = clock();
         {
-            for (int i = 0; i < len; ++i)
+            for (int i = 0; i < len/2; ++i)
             {
                 sum += arr[i];
             }
         }
+        clock_t endsum = clock();
     }
     free(arr);
     clock_t end = clock();
     printf("%lf",sum);
-    printf("\nThe elapsed time is %lf seconds", (double)(end - begin) / CLOCKS_PER_SEC);
+    printf("\nProgram running time = %lf seconds", (double)(end - begin) / CLOCKS_PER_SEC);
+    printf("\nFill cycle running time = %lf seconds", (double)(endfill - beginfill) / CLOCKS_PER_SEC);
+    printf("\nSum cycle running time = %lf seconds", (double)(endsum - beginsum) / CLOCKS_PER_SEC);
     return 0;
 }
