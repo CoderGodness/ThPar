9f91c7a72180d3a8e45d6407a0db73855a282617
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-22 13:32:36 +0700

    Update double.c

--- double.c
+++ double.c
@@ -14,7 +14,7 @@ int main()
     double sum = 0;
     #pragma acc data create(temp[0:len]) copyout(sum)
     {
-        #pragma acc parallel num_gangs(2048) 
+        #pragma acc parallel vector_length(256)
         {
             for (int i = 0; i < len; ++i)
             {
@@ -22,7 +22,7 @@ int main()
             }
         }
     
-        #pragma acc parallel num_gangs(2048)
+        #pragma acc parallel vector_length(256)
         {
             for (int i = 0; i < len; ++i)
             {
