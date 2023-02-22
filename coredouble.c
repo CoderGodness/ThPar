db69ca482f1ac7f2f0dc3fec9da10aced2297165
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-22 14:03:53 +0700

    Update coredouble.c

--- coredouble.c
+++ coredouble.c
@@ -3,32 +3,36 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h> 
+void fill(double* arr, int len)
+{
+     arr = (double*)malloc(sizeof(double) * len);
+     double x = 2 * M_PI / len;
+     for (int i = 0; i < len; ++i)
+    {
+        arr[i] = sin(i * x);
+    }
+}
+dobule summ(double* arr, int len)
+{
+    double sum=0;
+    double x = 2 * M_PI / len;
+    for (int i = 0; i < len/2; ++i)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
 
 int main()
 {
     clock_t begin = clock();
     int len = 10000000;
-    double x = 2 * M_PI / len;
-    double* arr = (double*)malloc(sizeof(double) * len);
-    double sum = 0;
-    clock_t beginfill = clock();
-    for (int i = 0; i < len; ++i)
-    {
-        arr[i] = sin(i * x);
-    }
-    clock_t endfill = clock();
-
-    clock_t beginsum = clock();
-    for (int i = 0; i < len/2; ++i)
-    {
-        sum += arr[i];
-    }
-    clock_t endsum = clock();
+    
+    double* arr;
+    fill(arr,len);
+    printf("%lf",summ(arr, len));
     free(arr);
     clock_t end = clock();
-    printf("%lf",sum);
     printf("\nProgram running time = %lf seconds", (double)(end - begin) / CLOCKS_PER_SEC);
-    printf("\nFill cycle running time = %lf seconds", (double)(endfill - beginfill) / CLOCKS_PER_SEC);
-    printf("\nSum cycle running time = %lf seconds", (double)(endsum - beginsum) / CLOCKS_PER_SEC);
     return 0;
 }
