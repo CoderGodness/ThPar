fffeee3ab6304194a0d372b276a74893bf460d68
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-22 16:30:21 +0700

    Update corefloat.c

--- corefloat.c
+++ corefloat.c
@@ -1,38 +1,34 @@
+#define _USE_MATH_DEFINES
 #include <malloc.h>
 #include <stdio.h>
 #include <math.h>
-#include <time.h>
-#define N 10000000
-#define M_PI 3.14159265358979323846
-void func(float** my_array, int len)
-{
-    float x = 2 * M_PI / N;
-    float* temp = (float*)malloc(sizeof(float) * len);
-      for (int i = 0; i < len; ++i)
-      {
-          temp[i] = sinf(i * x);
-      }
-    *my_array = temp;
-}
-float summ(float** my_array, int len)
-{
-    float sum = 0;
-    float* temp = *my_array;
-			for (int i = 0; i < len; ++i)
-			{
-					sum += temp[i];
-			}
-    return sum;
-}
+#include <time.h> 
 
 int main()
 {
- 		clock_t begin = clock();
-    float** array = (float**)malloc(sizeof(float**));
-    func(array, N);
-    printf("%lf\n", summ(array, N));
-    free(*array);
-		clock_t end = clock();
-		printf("The elapsed time is %lf seconds", (double)(end - begin) / CLOCKS_PER_SEC);
+    clock_t begin = clock();
+    int len = 10000000;
+    float x = 2 * M_PI / len;
+    float* arr = (float*)malloc(sizeof(float) * len);
+    float sum = 0;
+    clock_t beginfill = clock();
+    for (int i = 0; i < len; ++i)
+    {
+        arr[i] = sinf(i * x);
+    }
+    clock_t endfill = clock();
+
+    clock_t beginsum = clock();
+    for (int i = 0; i < len; ++i)
+    {
+        sum += arr[i];
+    }
+    clock_t endsum = clock();
+    free(arr);
+    clock_t end = clock();
+    printf("%f",sum);
+    printf("\nProgram running time = %lf seconds", (double)(end - begin) / CLOCKS_PER_SEC);
+    printf("\nFill cycle running time = %lf seconds", (double)(endfill - beginfill) / CLOCKS_PER_SEC);
+    printf("\nSum cycle running time = %lf seconds", (double)(endsum - beginsum) / CLOCKS_PER_SEC);
     return 0;
 }
