88c9f090f9325e5dbd96fbcd901a3f5ac98ef478
Coder <115636784+CoderGodness@users.noreply.github.com>
2023-02-22 14:07:54 +0700

    Update coredouble.c

--- coredouble.c
+++ coredouble.c
@@ -5,18 +5,17 @@
 #include <time.h> 
 void fill(double* arr, int len)
 {
-     arr = (double*)malloc(sizeof(double) * len);
-     double x = 2 * M_PI / len;
-     for (int i = 0; i < len; ++i)
+    
+    double x = 2 * M_PI / len;
+    for (int i = 0; i < len; ++i)
     {
         arr[i] = sin(i * x);
     }
 }
 double summ(double* arr, int len)
 {
-    double sum=0;
-    double x = 2 * M_PI / len;
-    for (int i = 0; i < len/2; ++i)
+    double sum = 0;
+    for (int i = 0; i < len; ++i)
     {
         sum += arr[i];
     }
@@ -27,10 +26,10 @@ int main()
 {
     clock_t begin = clock();
     int len = 10000000;
-    
-    double* arr;
-    fill(arr,len);
-    printf("%lf",summ(arr, len));
+
+    double* arr = (double*)malloc(sizeof(double) * len);
+    fill(arr, len);
+    printf("%lf", summ(arr, len));
     free(arr);
     clock_t end = clock();
     printf("\nProgram running time = %lf seconds", (double)(end - begin) / CLOCKS_PER_SEC);
